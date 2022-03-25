fb5048e9dfcc76a6844633cfccbd904733e3efad
+ <+>
2022-03-25 13:19:54 -0400

    commit tree 870c287f8f3757c1907fe4775447d0b985f99f2a

--- src/index.js
+++ src/index.js
@@ -1,1 +1,97 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener('DOMContentLoaded', function fetchDogs(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+    .then(dogData => renderDogs(dogData))
+
+})
+
+    function renderDogs(dogs) {
+    dogs.message.forEach(dogUrl => {
+        const 
+        dogImg = document.createElement('img')
+            dogImg.src = dogUrl
+
+        dogImg.style.maxWidth = '400px'
+       document.getElementById('dog-image-container').append(dogImg)
+
+    })
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+fetch(breedUrl)
+.then(res => res.json())
+.then(breedData => { 
+    const breedArray = Object.keys(breedData.message)
+
+    const breedContainer = document.querySelector('#dog-breeds')
+
+    breedArray.forEach(text => {
+        const newList = document.createElement('li')
+
+        newList.textContent = text;
+
+        breedContainer.append(newList)
+
+        newList.addEventListener('click', e => {
+            newList.style.color = 'green'
+    })
+
+
+
+    const breedDropdown = document.querySelector('select')
+
+    breedDropdown.addEventListener('change', filterBreeds);
+    
+    function filterBreeds(event) {
+       let letter = event.target.value
+
+      let breedFiltered =  breedArray.filter((breed) => {
+           return breed[0] === letter
+
+        //  const ul = document.querySelector('ul')
+
+
+
+       })
+
+       console.log(breedFiltered)
+
+       breedContainer.innerHTML = "";
+
+       filterBreeds(breedFiltered)
+
+    }}
+
+    
+
+       
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+    )}
+   
+
+
+
+
+
+
+
+
+
+
+)}
